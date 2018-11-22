17dccc1c0b598397b2526c9dfe815ef85360f003
utk09 <utkarsh.tiwari@somaiya.edu>
2018-11-22 09:06:47 +0530

    Update 2_variables.py

--- 2_Variables/2_variables.py
+++ 2_Variables/2_variables.py
@@ -15,9 +15,6 @@ print(type(firstYear))
 firstYear = "We are First Year Student's"  # String
 print(firstYear)
 
-firstYear = 'We are First Year Student''\s'  # String
-print(firstYear)
-
 anotherString = '2'
 just_another_string = "5"
 # print(anotherString + just_another_string)
