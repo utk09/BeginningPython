48b9822203be83d2d4df772daf2eb667dbc6f571
utk09 <utkarsh.tiwari@somaiya.edu>
2018-11-22 08:51:22 +0530

    1_Syntaxes added. Covers basic operations.

---
+++ 1_BasicSyntax/1_Syntaxes.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+print(8 + 3)  # Addition
+print(8 - 3)  # Subtraction
+print(8 * 3)  # Multiplication
+print(8 / 3)  # Division
+
+print(8 // 3)  # Quotient
+print(8 % 3)  # Remainder
+print(8 ** 3)  # Power(raised to)
+
+
+print("Addition:", 8 + 3)
+print("Power:", 8 ** 3)
