3e4c5f65d2a7d5c0a88b2bd50bff3e2ad93c3d1a
utk09 <utkarsh.tiwari@somaiya.edu>
2018-11-30 22:17:38 +0530

    Update

--- 8_MiniPrograms/17_simpleMaths.py
+++ 8_MiniPrograms/17_simpleMaths.py
@@ -4,6 +4,32 @@ import sys
 # The libraries are pre-written functions which can be directly used.
 # sys is a library for "system"
 
-line = int(input())
-split = line.split()
+print("Enter a mathematical expression. Make sure you've spaces in between numbers and operators. Eg: 5 * 6")
+many_inputs = input()  # Type in format 5 + 4 and it'll give you answer. You can also do 5 - 3, 6 * 5 and so on.
+splitting = many_inputs.split()
+
+left_val = int(splitting[0]) # we're doing indexing to take out left value, operator used and right value
+operator = splitting[1]
+right_val = int(splitting[2])
+
+# answer = 0
+
+if operator == '+':
+    print("Addition:", left_val + right_val)
+elif operator == '-':
+    print("Subtraction:", left_val - right_val)
+elif operator == '*':
+    print("Multiplication:", left_val*right_val)
+elif operator == '**' or operator == '^':
+    print("Power:", left_val**right_val)
+elif operator == "/":
+    if right_val is not 0:
+        print("Division:", left_val/right_val)
+    else:
+        print("Division by 0 not possible")
+        sys.exit()
+else:
+    print("Unknown Operator detected. We don't know about {opr}".format(opr = operator))
+
+
 
