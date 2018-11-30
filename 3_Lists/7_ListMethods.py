b135f2b2c83a9a921eeb5606e31e2bdbc5060264
utk09 <utkarsh.tiwari@somaiya.edu>
2018-11-30 14:48:01 +0530

    Update

--- 3_Lists/7_ListMethods.py
+++ 3_Lists/7_ListMethods.py
@@ -8,7 +8,7 @@ print(numbers_list, "\n", alphabet_list, "\n", jumbled_list)
 numbers_list.append(781)  # It adds the value at the end of the list.
 print(numbers_list)
 
-numbers_list = numbers_list + [69]  # another way to append
+numbers_list = numbers_list + [69, 9.81]  # another way to append
 print(numbers_list)
 
 alphabet_list.pop(3)  # removes the value at "INDEX 3"
