091b13d36556eafada205085e401b0efc616de5f
utk09 <utkarsh.tiwari@somaiya.edu>
2018-11-30 23:02:37 +0530

    Update 11_WhileLoop.py

--- 6_Loops/11_WhileLoop.py
+++ 6_Loops/11_WhileLoop.py
@@ -3,8 +3,11 @@ fruits = ["apple", "mango", "strawberry", "grapes", "pear", "kiwi", "orange", "b
 print(fruits)
 print(len(fruits))
 
-while index < len(fruits):
+while index < len(fruits): # while loop goes on executing until the condition inside it is satisfied
     fruit_new = fruits[index]
     print(index)
     print(fruit_new)
     index = index + 1
+
+# while True:  # this is an infinite loop
+#   print("a")
