346d23e29f2241bb4abae0dca75526bee8a48b52
utk09 <utkarsh.tiwari@somaiya.edu>
2018-11-30 23:00:02 +0530

    Update

---
+++ 6_Loops/11_WhileLoop.py
@@ -0,0 +1,10 @@
+index = 0
+fruits = ["apple", "mango", "strawberry", "grapes", "pear", "kiwi", "orange", "banana"]
+print(fruits)
+print(len(fruits))
+
+while index < len(fruits):
+    fruit_new = fruits[index]
+    print(index)
+    print(fruit_new)
+    index = index + 1
