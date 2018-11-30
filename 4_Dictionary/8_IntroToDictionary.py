54ece0f1ec53e285d2d576499ca97c4517468e04
utk09 <utkarsh.tiwari@somaiya.edu>
2018-11-30 16:09:45 +0530

    Update

---
+++ 4_Dictionary/8_IntroToDictionary.py
@@ -0,0 +1,18 @@
+# Dictionaries are Key:value pairs.
+# Example: Our mobile phone directory.
+# The Key is name and Value is phone number
+
+car_companies = {
+    "Toyota": "Japan",
+    "General Motors":"USA",
+    "Volkswagen":"Germany",
+    "Ford":"USA",
+    "Honda":"Japan",
+    "PSA Peugeot Citreon":"France",
+    "Hyundai-Kia":"South Korea",
+    "Tata Motors":"India",
+    "Mercedes Benz":"Germany",
+    "Suzuki Motor Corporation":"Japan"
+}
+
+print(car_companies)
