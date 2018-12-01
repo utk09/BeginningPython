7ab6a1b95db55a2cc3caeecd4195acb9491d41fc
utk09 <utkarsh.tiwari@somaiya.edu>
2018-12-01 15:34:04 +0530

    Update

--- 8_MiniPrograms/13_PrimeNumberChecker.py
+++ 8_MiniPrograms/13_PrimeNumberChecker.py
@@ -1,5 +1,7 @@
 # This program will take input from User and tell whether it's Prime or Not.
 
+import numpy as np
+
 print('''
 Hey! Enter a number and I'll tell you if it's prime or not.
 ''')
@@ -8,7 +10,8 @@ num = int(input("Enter a number: "))
 if num >= 2:
     divisors_list = []
     for divisor in range (2, num):
-        if num % divisor == 0:
+        # if num % divisor == 0:
+        if np.remainder(num, divisor) == 0:
             divisors_list.append(divisor)
 
     if len(divisors_list) == 0:
