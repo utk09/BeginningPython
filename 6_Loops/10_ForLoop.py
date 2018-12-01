22dd72da97736b247846f31d6ca26a81d9116e9a
utk09 <utkarsh.tiwari@somaiya.edu>
2018-12-01 10:21:34 +0530

    Update

--- 6_Loops/10_ForLoop.py
+++ 6_Loops/10_ForLoop.py
@@ -1,10 +1,16 @@
 '''
-In a list of numbers from 1 to 100, make a list of numbers which are divisible by the number user inputs.
+In a list of numbers from 1 to 100, make a list of numbers which are divisible by,
+the number which user inputs.
 '''
 
 list1 = []
 list2 = []
 for number in range(1, 101):
+    '''
+    To loop through a set of code a specified number of times, we can use the range() function,
+    The range() function returns a sequence of numbers, starting from 0 by default, 
+    and increments by 1 (by default), and ends at a specified number.
+    '''
     list1.append(number)
 print(list1)
 
@@ -16,6 +22,11 @@ for divNum in list1:
     if (divNum % divisor == 0 and divNum != divisor):
         count += 1
         list2.append(divNum)
-print("Output List: ",list2)
+print("Output List: ", list2)
 
-print("Count is: ",count)
+print("Count is: ", count)
+
+'''
+A "for" loop is used for iterating over a sequence 
+that is either a list, a tuple, a dictionary, a set, or a string.
+'''
--- 6_Loops/11_WhileLoop.py
+++ 6_Loops/11_WhileLoop.py
@@ -9,5 +9,11 @@ while index < len(fruits): # while loop goes on executing until the condition in
     print(fruit_new)
     index = index + 1
 
-# while True:  # this is an infinite loop
+# while True:  # Uncomment this, it is an infinite loop
 #   print("a")
+
+'''
+The "while" loop can execute a set of statements as long as a condition is true.
+The "break" statement can stop the loop even if the while condition is true.
+The continue statement can stop the current iteration, and continue with the next.
+'''
